7839ad1534abe70623dcba242c32931b15cf0409
Marco <pastry07@gmail.com>
2023-01-23 15:42:01 -0800

    added img_URL to the model

--- seed.js
+++ seed.js
@@ -11,11 +11,13 @@ async function seed()
 {
     // **title: {type: String, required: true},
     // **descripton: {type: String, required: true},
+    // **img_URL: { type: String, required: true },
     // **status: {type: Boolean, required: true}
 
     await Book.create({
         title: 'Moby Dick',
         descripton: 'Life long hunt for the giant white whale',
+        img_URL: 'https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg',
         status: true
     });
 
@@ -24,6 +26,7 @@ async function seed()
     await Book.create({
         title: 'James and the Gaint Peach',
         descripton: 'Orpahn enbarks on journey to New York in a gaint peach, with his new buggy friends',
+        img_URL: 'https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg',
         status: true
     });
 
@@ -32,6 +35,7 @@ async function seed()
     await Book.create({
         title: 'Huckleberry Finn',
         descripton: 'Huckleberry enbarks many adventures with his good friend Tom Sawyer',
+        img_URL: 'https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg',
         status: true
     });
 
