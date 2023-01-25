141ff2265e1ef6113d29773d5a9d8d735d432a5c
Marco <pastry07@gmail.com>
2023-01-24 16:38:50 -0800

    added postBook

--- seed.js
+++ seed.js
@@ -10,13 +10,13 @@ const Book = require('./models/book.js');
 async function seed()
 {
     // **title: {type: String, required: true},
-    // **descripton: {type: String, required: true},
+    // **description: {type: String, required: true},
     // **img_URL: { type: String, required: true },
     // **status: {type: Boolean, required: true}
 
     await Book.create({
         title: 'Moby Dick',
-        descripton: 'Life long hunt for the giant white whale',
+        description: 'Life long hunt for the giant white whale',
         img_URL: 'https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg',
         status: true
     });
@@ -25,7 +25,7 @@ async function seed()
 
     await Book.create({
         title: 'James and the Gaint Peach',
-        descripton: 'Orpahn enbarks on journey to New York in a gaint peach, with his new buggy friends',
+        description: 'Orpahn enbarks on journey to New York in a gaint peach, with his new buggy friends',
         img_URL: 'https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg',
         status: true
     });
@@ -34,7 +34,7 @@ async function seed()
 
     await Book.create({
         title: 'Huckleberry Finn',
-        descripton: 'Huckleberry enbarks many adventures with his good friend Tom Sawyer',
+        description: 'Huckleberry enbarks many adventures with his good friend Tom Sawyer',
         img_URL: 'https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg',
         status: true
     });
